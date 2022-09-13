ddb1e49400a0a05b848b22b0e32bb0674b99ca4a
Danae <danae_ai102_sep2022@outlook.com>
2022-09-13 06:18:33 -0700

    Completed Lab08

--- 08-speech-translation/C-Sharp/translator/Program.cs
+++ 08-speech-translation/C-Sharp/translator/Program.cs
@@ -5,7 +5,10 @@ using System.Collections.Generic;
 using System.Text;
 
 // Import namespaces
-
+// Import namespaces
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech.Translation;
 
 namespace speech_translation
 {
@@ -26,15 +29,23 @@ namespace speech_translation
 
 
                 // Configure translation
-
+                // Configure translation
+                translationConfig = SpeechTranslationConfig.FromSubscription(cogSvcKey, cogSvcRegion);
+                translationConfig.SpeechRecognitionLanguage = "en-US";
+                translationConfig.AddTargetLanguage("fr");
+                translationConfig.AddTargetLanguage("es");
+                translationConfig.AddTargetLanguage("hi");
+                translationConfig.AddTargetLanguage("el");
+                Console.WriteLine("Ready to translate from " + translationConfig.SpeechRecognitionLanguage);
 
                 // Configure speech
-                
+                // Configure speech
+                speechConfig = SpeechConfig.FromSubscription(cogSvcKey, cogSvcRegion);
 
                 string targetLanguage = "";
                 while (targetLanguage != "quit")
                 {
-                    Console.WriteLine("\nEnter a target language\n fr = French\n es = Spanish\n hi = Hindi\n Enter anything else to stop\n");
+                    Console.WriteLine("\nEnter a target language\n fr = French\n es = Spanish\n hi = Hindi\n el = Greek\n Enter anything else to stop\n");
                     targetLanguage=Console.ReadLine().ToLower();
                     if (translationConfig.TargetLanguages.Contains(targetLanguage))
                     {
@@ -57,10 +68,32 @@ namespace speech_translation
             string translation = "";
 
             // Translate speech
-
+            // Translate speech
+            using AudioConfig audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using TranslationRecognizer translator = new TranslationRecognizer(translationConfig, audioConfig);
+            Console.WriteLine("Speak now...");
+            TranslationRecognitionResult result = await translator.RecognizeOnceAsync();
+            Console.WriteLine($"Translating '{result.Text}'");
+            translation = result.Translations[targetLanguage];
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine(translation);
 
             // Synthesize translation
-
+            // Synthesize translation
+            var voices = new Dictionary<string, string>
+                            {
+                                ["fr"] = "fr-FR-HenriNeural",
+                                ["es"] = "es-ES-ElviraNeural",
+                                ["hi"] = "hi-IN-MadhurNeural",
+                                ["el"] = "el-GR-AthinaNeural"
+                            };
+            speechConfig.SpeechSynthesisVoiceName = voices[targetLanguage];
+            using SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig);
+            SpeechSynthesisResult speak = await speechSynthesizer.SpeakTextAsync(translation);
+            if (speak.Reason != ResultReason.SynthesizingAudioCompleted)
+            {
+                Console.WriteLine(speak.Reason);
+            }
 
         }
 
