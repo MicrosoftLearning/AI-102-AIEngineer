870ebf03db6df36e3b2535d0cd970e1306f8638b
Adnan Abdulle <51664265+adnancore@users.noreply.github.com>
2023-10-13 15:54:07 +0100

    added bash files

---
+++ 03-monitor/rest-test.sh
@@ -0,0 +1,1 @@
+curl -X POST "<your-endpoint>/text/analytics/v3.1/languages?" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: <your-key>" --data-ascii "{'documents':           [{'id':1,'text':'hello'}]}"
