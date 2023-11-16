0d47209023e69a3422844da56ae23962fc9903b0
Graeme Malcolm <gmalc@microsoft.com>
2023-11-16 09:44:35 -0500

    Removed public blob

--- 22-Create-a-search-solution/UploadDocs.cmd
+++ 22-Create-a-search-solution/UploadDocs.cmd
@@ -8,7 +8,7 @@ set azure_storage_key=YOUR_AZURE_STORAGE_KEY
 
 
 echo Creating container...
-call az storage container create --account-name !azure_storage_account! --subscription !subscription_id! --name margies --public-access blob --auth-mode key --account-key !azure_storage_key! --output none
+call az storage container create --account-name !azure_storage_account! --subscription !subscription_id! --name margies --auth-mode key --account-key !azure_storage_key! --output none
 
 echo Uploading files...
 call az storage blob upload-batch -d margies -s data --account-name !azure_storage_account! --auth-mode key --account-key !azure_storage_key!  --output none
