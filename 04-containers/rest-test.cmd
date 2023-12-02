e8eae040851ebe5fea807cda540e70f0a69daa94
Rokas Višinskas <rokas.post@gmail.com>
2023-12-02 20:35:41 +0200

    Update 04-containers/rest-test.cmd
    The existing curl command is missing the http method argument '-X'.
    
    This causes the error:
    "curl: (6) Could not resolve host: POST"

--- 04-containers/rest-test.cmd
+++ 04-containers/rest-test.cmd
@@ -1,1 +1,1 @@
-curl POST "http://<your_ACI_IP_address_or_FQDN>:5000/text/analytics/v3.0/languages" -H "Content-Type: application/json" --data-ascii "{'documents':[{'id':1,'text':'Hello world.'},{'id':2,'text':'Salut tout le monde.'}]}"
+curl -X POST "http://<your_ACI_IP_address_or_FQDN>:5000/text/analytics/v3.0/languages" -H "Content-Type: application/json" --data-ascii "{'documents':[{'id':1,'text':'Hello world.'},{'id':2,'text':'Salut tout le monde.'}]}"
